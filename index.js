9132e889c917d43dcbd67728ac3dab53031d3fe3
Kat Michaela <k@gothfem.me>
2018-10-05 14:01:33 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,36 @@
 // Code your solution in this file!
+function distanceFromHqInBlocks (someNumber) {
+  let result
+  someNumber >= 42 ? result = someNumber - 42 : result = 42 - someNumber
+  return result
+}
+
+function distanceFromHqInFeet (someNumber) {
+  let result = distanceFromHqInBlocks(someNumber) * 264
+  return result
+}
+
+function distanceTravelledInFeet (valueOne, valueTwo) {
+  let result
+  if (valueOne >= valueTwo) {
+    result = (valueOne - valueTwo) * 264
+  } else {
+    result = (valueTwo - valueOne) * 264
+  }
+  return result
+}
+
+function calculatesFarePrice (start, destination) {
+  let result
+  let feet = distanceTravelledInFeet(start, destination)
+  if (feet <= 400) {
+    result = 0.0
+  } else if (feet <= 2000) {
+    result = (feet - 400) * 0.02
+  } else if (feet > 2000 && feet <= 2500) {
+    result = 25
+  } else {
+    result = `cannot travel that far`
+  }
+  return result
+}
